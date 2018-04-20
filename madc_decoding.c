f3f0813cd19f23915a03c2c8d2082228d53d97ff
Huagen Xu <h.xu@fz-juelich.de>
2018-04-20 15:09:45 +0200

    update

--- madc_decoding.c
+++ madc_decoding.c
@@ -640,12 +640,12 @@ int decoding(int f)
                      //check the dt is abnormal to the expected;
                      if(abs(abs(dt2)-691)>5||abs(abs(dt3)-1555)>5||abs(abs(dt4)-2575)>5||abs(abs(dt5)-3253)>5||abs(abs(dt6)-4180)>5){
                        cout<<"The time stamp is abnormal"<<endl;
-                       cout<<"the dt1= "<<dt1<<" TS1= "<<data2[1][0]<<endl
-                           <<"the dt2= "<<dt2<<" TS2= "<<data2[2][0]<<endl
-                           <<"the dt3= "<<dt3<<" TS3= "<<data2[3][0]<<endl
-                           <<"the dt4= "<<dt4<<" TS4= "<<data2[4][0]<<endl
-                           <<"the dt5= "<<dt5<<" TS5= "<<data2[5][0]<<endl
-                           <<"the dt6= "<<dt6<<" TS6= "<<data2[6][0]<<endl;
+                       cout<<"the dt1= "<<dt1<<" TS1= "<<data2[1][0]<<" "<<time_evt_ADC1<<endl
+                           <<"the dt2= "<<dt2<<" TS2= "<<data2[2][0]<<" "<<time_evt_ADC2<<endl
+                           <<"the dt3= "<<dt3<<" TS3= "<<data2[3][0]<<" "<<time_evt_ADC3<<endl
+                           <<"the dt4= "<<dt4<<" TS4= "<<data2[4][0]<<" "<<time_evt_ADC4<<endl
+                           <<"the dt5= "<<dt5<<" TS5= "<<data2[5][0]<<" "<<time_evt_ADC5<<endl
+                           <<"the dt6= "<<dt6<<" TS6= "<<data2[6][0]<<" "<<time_evt_ADC6<<endl;
                      }
 
 			             } else continue;  //module ID
@@ -935,7 +935,7 @@ int openfile(char *file){
 
       //  printf("In openfile(): (decoding status) res=decoding(f) is %d \n", res);
 		if(count%1 == 0) cout<<"The No of "<<count<<" clusters have been decoded"<<endl;
-	} while (count++<200&&res ==0 );
+	} while (count++<170&&res ==0 );
 
 	//close(f);
        printf("In openfile():   close the file!\n");
