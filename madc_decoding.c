8d5978bcc92a21b9c0032d6138d305cb64be63ad
Huagen Xu <h.xu@fz-juelich.de>
2018-04-20 10:09:49 +0200

    update

--- madc_decoding.c
+++ madc_decoding.c
@@ -920,7 +920,7 @@ int openfile(char *file){
 
       //  printf("In openfile(): (decoding status) res=decoding(f) is %d \n", res);
 		if(count%1000 == 0) cout<<"The No of "<<count<<" clusters have been decoded"<<endl;
-	} while (count++<1&&res ==0 );
+	} while (count++<2&&res ==0 );
 
 	//close(f);
        printf("In openfile():   close the file!\n");
